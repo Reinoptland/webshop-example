0d73e7e96bb953832023e715de079571645d827a
reinoptland <reinoptland@gmail.com>
2019-11-15 09:48:36 +0100

    Setup with api & Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+
+class Home extends Component {
+  render() {
+    return <div>THIS IS HOME</div>;
+  }
+}
+
+class ProductList extends Component {
+  render() {
+    return <div>PRODUCT LIST</div>;
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={ProductList} />
       </header>
     </div>
   );
